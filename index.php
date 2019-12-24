4825a97d6721e5668a334d2a3b84bc718326bd39
feriida11 <feriidafirmanto@gmail.com>
2019-12-24 14:32:38 +0700

    menambahkan file index.php

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial">
+	<meta http-equiv="X-UA-compatible" content="ie=edge">
+	<title>Coba Git Remote</title>
+</head>
+<body>
+	<h1>hello world</h1>
+	
+</body>
+</html>
